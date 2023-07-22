f658055244097fe584d1a477ec83c9a08dc45b3f
skinmaker1345 <me@skinmaker.dev>
2023-07-23 02:06:48 +0900

    fix: unflag on unmodified

--- index.js
+++ index.js
@@ -2,51 +2,58 @@ import fs from "fs";
 
 const translations = new Map();
 
-
 const old = fs.readFileSync("./old.po", "utf8");
 const template = fs.readFileSync("./template.po", "utf8");
 
-
 let txt = "";
 old.split("\n").forEach((line) => {
-    const [key, ...v] = line.split(" ");
-    if(v.length) {
-        const value = v.join(" ");
-        switch(key) {
-            case "msgctxt":
-                txt = value;
-                break;
-            case "msgstr":
-                if(/[ㄱ-ㅎㅏ-ㅣ가-힣]/.test(value)) {
-                    translations.set(txt, value);
-                }
-                break;
-        }
-    }
+	const [key, ...v] = line.split(" ");
+	if (v.length) {
+		const value = v.join(" ");
+		switch (key) {
+			case "msgctxt":
+				txt = value;
+				break;
+			case "msgstr":
+				if (/[ㄱ-ㅎㅏ-ㅣ가-힣]/.test(value)) {
+					translations.set(txt, value);
+				}
+				break;
+		}
+	}
 });
 
 let id = "";
 let flagged = false;
 
-const newFile = Buffer.from(template.split("\n").map((line) => {
-    if(line.includes("modified")) {
-        flagged = true;
-    }
-    const [key, ...v] = line.split(" ");
-    if(v.length) {
-        const value = v.join(" ");
-        switch(key) {
-            case "msgctxt":
-                txt = value;
-                break;
-            case "msgid":
-                id = value;
-                break;
-            case "msgstr":
-                return flagged ? `msgstr ${id}` : `msgstr ${translations.get(txt) ?? id}`
-        }
-    }
-    return line;
-}).join("\n"));
+const newFile = Buffer.from(
+	template
+		.split("\n")
+		.map((line) => {
+			if (line.includes("unmodified")) {
+				flagged = false;
+			} else if (line.includes("modified")) {
+				flagged = true;
+			}
+			const [key, ...v] = line.split(" ");
+			if (v.length) {
+				const value = v.join(" ");
+				switch (key) {
+					case "msgctxt":
+						txt = value;
+						break;
+					case "msgid":
+						id = value;
+						break;
+					case "msgstr":
+						return flagged
+							? `msgstr ${id}`
+							: `msgstr ${translations.get(txt) ?? id}`;
+				}
+			}
+			return line;
+		})
+		.join("\n")
+);
 
 fs.writeFileSync("./new.po", newFile);
